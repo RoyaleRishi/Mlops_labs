e4742050553d39f7c7c55c0348feb9470d1330f7
rishis <royalera.works@gmail.com>
2026-01-30 00:41:47 -0500

    lab1

---
+++ Lab1/src/main.py
@@ -0,0 +1,22 @@
+# Import necessary libraries
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+if __name__ == '__main__':
+    # Load the Wine dataset
+    data = load_wine()
+    X, y = data.data, data.target
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Train a Random Forest classifier
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+
+    # Save the model to a file
+    joblib.dump(model, 'wine_model.pkl')
+    
+    print("Themodel training was successful")
